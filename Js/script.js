09acfd74a876c73f9c462b1e93a23b40a34e8751
Ark21bit <webleif@vk.com>
2022-08-19 19:54:08 +0300

    модалка готова

--- Js/script.js
+++ Js/script.js
@@ -5,3 +5,35 @@ $( "#accordion" ).accordion({
 $( "#tabs" ).tabs({
     active: 2
 });
+
+$(document).ready(function($) {
+	$('.btn_payment').click(function() {
+		$('.modal').fadeIn();
+		return false;
+	});	
+
+	$('.btn_payment').click(function() {
+		$('.overlay').fadeIn();
+		return false;
+	});	
+	
+	$('.close_modal').click(function() {
+		$(this).parents('.modal').fadeOut();
+		return false;
+	});	
+
+	$('.close_modal').click(function() {
+		$('.overlay').fadeOut();
+		return false;
+	});	
+
+	$('.overlay').click(function() {
+		$('.modal').fadeOut();
+		return false;
+	});		
+
+	$('.overlay').click(function() {
+		$('.overlay').fadeOut();
+		return false;
+	});	
+});
